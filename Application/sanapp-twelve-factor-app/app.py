e9cafdeb5862c70ed1747a1de6791625be02b6d1
Sandeep-Doebar <86931071+Sandeep-Doebar@users.noreply.github.com>
2023-09-30 21:56:48 +0200

    Added redis db changes

--- Application/sanapp-twelve-factor-app/app.py
+++ Application/sanapp-twelve-factor-app/app.py
@@ -1,10 +1,17 @@
 #code for basic python app
-from flask import Flask, render_template
+from flask import Flask
+from redis import Redis
+
 app = Flask(__name__)
+redisDb = Redis(host=os.getenv('HOST'), port=os.getenv('PORT'))
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def Santwelvefactorapp():
+    redisDb.incr('visitorCount')
+    visitCount = str(redisDb.get('visitorCount'), 'utf-8')
+    global visitCount
+    visitCount+=1
+    return "Welcome to my twelvefactorapp! Visitor Count: " + str(visitCount)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True) 
