b2a4b7c16486f21e82a1052284238606f9a4ef48
Sandeep-Doebar <86931071+Sandeep-Doebar@users.noreply.github.com>
2023-10-04 19:04:26 +0200

    redis stateful changes

--- Application/sanapp-twelve-factor-app/app.py
+++ Application/sanapp-twelve-factor-app/app.py
@@ -1,17 +1,16 @@
 #code for basic python app
+import redis
 from flask import Flask
-from redis import Redis
+
 
 app = Flask(__name__)
-redisDb = Redis(host=os.getenv('HOST'), port=os.getenv('PORT'))
+redisDb = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def Santwelvefactorapp():
     redisDb.incr('visitorCount')
     visitCount = str(redisDb.get('visitorCount'), 'utf-8')
-    global visitCount
-    visitCount+=1
-    return "Welcome to my twelvefactorapp! Visitor Count: " + str(visitCount)
+    return "Welcome to my twelvefactorapp! Visitor Count: " + visitCount
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True) 
